7c9e9bde70734cbddac49ae2daa7e2fbd8478be5
Justin Sherrill <jsherril@redhat.com>
2022-04-13 12:39:52 -0400

    message sending

--- server.go
+++ server.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	clowder "github.com/redhatinsights/app-common-go/pkg/api/v1"
+	kafka "github.com/segmentio/kafka-go"
 	"log"
 	"strconv"
 )
@@ -13,8 +18,58 @@ type Widget struct {
 	Id     int64  `form:"id"`
 }
 
+func getUrl() (string, error) {
+	cfg := clowder.LoadedConfig
+	if cfg.Kafka.Brokers[0].Hostname != "" {
+		return fmt.Sprintf("%s:%v", cfg.Kafka.Brokers[0].Hostname, *cfg.Kafka.Brokers[0].Port), nil
+	} else {
+		return "", errors.New("empty name")
+	}
+}
+
+func getTopic() (string, error) {
+	topic := "foo"
+	for _, topicConfig := range clowder.KafkaTopics {
+		topic = topicConfig.Name
+	}
+	if topic == "" {
+		return "", errors.New("empty name")
+	} else {
+		return topic, nil
+	}
+
+}
+
+func getKafkaWriter() *kafka.Writer {
+	kafkaUrl, _ := getUrl()
+	topic, _ := getTopic()
+	return &kafka.Writer{
+		Addr:     kafka.TCP(kafkaUrl),
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	}
+}
+
+func sendMessage(context context.Context, id int64, name string) {
+	if clowder.IsClowderEnabled() {
+		kafkaWriter := getKafkaWriter()
+
+		msg := kafka.Message{
+			Key:   []byte(fmt.Sprintf("id-%s", id)),
+			Value: []byte(name),
+		}
+		err := kafkaWriter.WriteMessages(context, msg)
+
+		if err == nil {
+			log.Fatalln("Sent message")
+		} else {
+			log.Fatalln(err)
+		}
+	}
+}
+
 func main() {
-	my_widgets := make(map[int64]Widget)
+	myWidgets := make(map[int64]Widget)
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -23,8 +78,8 @@ func main() {
 		})
 	})
 	r.GET("/widgets/", func(c *gin.Context) {
-		list := make([]Widget, 0, len(my_widgets))
-		for _, w := range my_widgets {
+		list := make([]Widget, 0, len(myWidgets))
+		for _, w := range myWidgets {
 			list = append(list, w)
 		}
 		c.JSON(http.StatusOK, list)
@@ -34,14 +89,15 @@ func main() {
 		if c.BindJSON(&widget) == nil {
 			log.Println(widget.Id)
 			log.Println(widget.Name)
-			my_widgets[widget.Id] = widget
+			sendMessage(c, widget.Id, widget.Name)
+			myWidgets[widget.Id] = widget
 
 			c.JSON(http.StatusOK, widget)
 		}
 	})
 	r.GET("/widgets/:id", func(c *gin.Context) {
 		id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
-		widget, found := my_widgets[id]
+		widget, found := myWidgets[id]
 		if found {
 			c.JSON(http.StatusOK, widget)
 		} else {
