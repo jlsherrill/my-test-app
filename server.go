a12f7778a89cfa92acd33fb16c7e8c1139a20e31
Justin Sherrill <jsherril@redhat.com>
2022-04-21 15:35:52 -0400

    gorm

--- server.go
+++ server.go
@@ -1,126 +1,29 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	clowder "github.com/redhatinsights/app-common-go/pkg/api/v1"
-	kafka "github.com/segmentio/kafka-go"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 	"log"
 	"strconv"
-	"time"
 )
 import "net/http"
 
 type Widget struct {
+	gorm.Model
 	Name   string `form:"name"`
 	Flavor string `form:"flavor"`
 	Id     int64  `form:"id"`
 }
 
-func getUrl() (string, error) {
-	cfg := clowder.LoadedConfig
-	if cfg.Kafka.Brokers[0].Hostname != "" {
-		return fmt.Sprintf("%s:%v", cfg.Kafka.Brokers[0].Hostname, *cfg.Kafka.Brokers[0].Port), nil
-	} else {
-		return "", errors.New("empty name")
-	}
-}
-
-func getTopic() (string, error) {
-	topic := "foo"
-	for _, topicConfig := range clowder.KafkaTopics {
-		topic = topicConfig.Name
-	}
-	if topic == "" {
-		return "", errors.New("empty name")
-	} else {
-		return topic, nil
-	}
-
-}
-
-func getKafkaWriter() (*kafka.Writer, error) {
-	if !clowder.IsClowderEnabled() {
-		return nil, errors.New("Clowder disabled")
-	}
-	kafkaUrl, urlError := getUrl()
-	if urlError != nil {
-		return nil, urlError
-	}
-	topic, topicError := getTopic()
-	if topicError != nil {
-		return nil, topicError
-	}
-	return &kafka.Writer{
-		Addr:     kafka.TCP(kafkaUrl),
-		Topic:    topic,
-		Balancer: &kafka.LeastBytes{},
-	}, nil
-}
-
-func sendMessage(context context.Context, writer *kafka.Writer, id int64, name string) {
-	if clowder.IsClowderEnabled() {
-		msg := kafka.Message{
-			Key:   []byte(fmt.Sprintf("id-%s", id)),
-			Value: []byte(name),
-		}
-		err := writer.WriteMessages(context, msg)
-
-		if err == nil {
-			log.Println("Sent message")
-		} else {
-			log.Fatalln(err)
-		}
-	} else {
-		log.Println("clowder disabled")
-	}
-}
-
-func listener() {
-	if !clowder.IsClowderEnabled() {
-		log.Println("clowder disabled")
-		return
-	}
-	kafkaUrl, urlError := getUrl()
-	if urlError != nil {
-		return
-	}
-	topic, topicError := getTopic()
-	if topicError != nil {
-		return
-	}
-	partition := 0
-	conn, err := kafka.DialLeader(context.Background(), "tcp", kafkaUrl, topic, partition)
-	if err != nil {
-		log.Fatal("failed to dial leader:", err)
-	}
-
-	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-	batch := conn.ReadBatch(10e3, 1e6)
-	b := make([]byte, 10e3) // 10KB max per message
-	for {
-		n, err := batch.Read(b)
-		if err != nil {
-			break
-		}
-		fmt.Println(string(b[:n]))
-	}
-}
-
 func apiServer() {
-	kafkaWriter, error := getKafkaWriter()
-	if error != nil {
-		log.Println("Could not initizlize kafka writer")
-		log.Println(error)
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
 	}
-	kafkaWriter.BatchTimeout, error = time.ParseDuration("100ms")
-	if error != nil {
-		log.Println(error)
-	}
+	db.AutoMigrate(&Widget{})
 
-	myWidgets := make(map[int64]Widget)
+	//myWidgets := make(map[int64]Widget)
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -129,29 +32,35 @@ func apiServer() {
 		})
 	})
 	r.GET("/widgets/", func(c *gin.Context) {
-		list := make([]Widget, 0, len(myWidgets))
-		for _, w := range myWidgets {
-			list = append(list, w)
-		}
+		//list := make([]Widget, 0, len(myWidgets))
+		//for _, w := range myWidgets {
+		//	list = append(list, w)
+		//}
+		var list []Widget
+		db.Find(&list)
 		c.JSON(http.StatusOK, list)
 	})
 	r.POST("/widgets/", func(c *gin.Context) {
 		var widget Widget
 		if c.BindJSON(&widget) == nil {
-			if widget.Name == "send" {
-				sendMessage(c, kafkaWriter, widget.Id, widget.Name)
-			}
 			log.Println(widget.Id)
 			log.Println(widget.Name)
-			myWidgets[widget.Id] = widget
+			if result := db.Create(&widget); result.Error != nil {
+				log.Println(result.Error)
+				c.JSON(http.StatusInternalServerError, "")
+			} else {
+				c.JSON(http.StatusOK, widget)
+			}
 
-			c.JSON(http.StatusOK, widget)
 		}
 	})
 	r.GET("/widgets/:id", func(c *gin.Context) {
+		var widget Widget
 		id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
-		widget, found := myWidgets[id]
-		if found {
+
+		result := db.Where("id = ?", id).First(&widget)
+		//widget, found := myWidgets[id]
+		if result.RowsAffected == 1 {
 			c.JSON(http.StatusOK, widget)
 		} else {
 			c.String(404, "Not Found")
